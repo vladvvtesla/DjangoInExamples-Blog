6f17ec53ee31489a2ffbd24bfc5f58e7dbd697db
Vladimir V. Vladimirov <mckeency@gmail.com>
2020-09-24 21:43:12 +0300

    New feature: adding comments to articles

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
 from django.core.mail import send_mail
 
-from .models import Post
-
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 def post_list(request):
     object_list = Post.published.all()
@@ -25,7 +24,25 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',publish__year=year,
                              publish__month=month,publish__day=day)
-    return render(request,'blog/post/detail.html',{'post': post})
+    # Список активных комментариев для этой статьи.
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # Пользователь отправил комментарий.
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Создаем комментарий, но пока не сохраняем в базе данных.
+            new_comment = comment_form.save(commit=False)
+            # Привязываем комментарий к текущей статье.
+            new_comment.post = post
+            # Сохраняем комментарий в базе данных.
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request,'blog/post/detail.html',{'post': post,
+                                                   'comments': comments,
+                                                   'new_comment': new_comment,
+                                                   'comment_form': comment_form})
 
 class PostListView(ListView):
     queryset = Post.published.all()
